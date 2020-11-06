f034537b332ac1bce404d41c5abac79d35963b36
Selma Elgabalawy <salmosha88@gmail.com>
2020-11-06 00:44:52 -0800

    updates

--- Develop/index.js
+++ Develop/index.js
@@ -1,16 +1,64 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown")
+
 // array of questions for user
-const questions = [
+// const questions = [
+// ];
+// function readmeDoc
 
-];
-
-// function to write README file
-function writeToFile(fileName, data) {
-}
-
-// function to initialize program
 function init() {
-
+    inquirer.prompt([{
+                type: "input",
+                message: "What is your full name?",
+                name: "name"
+            },
+            {
+                type: "input",
+                message: "What is the title of your project?",
+                name: "title"
+            },
+            {
+                type: "input",
+                message: "What is the description of your project?",
+                name: "description"
+            },
+            {
+                type: "input",
+                message: "What is the file name and extension of your usage screenshots?",
+                name: "usage"
+            },
+            {
+                type: "input",
+                message: "What are the contribution guidelines?",
+                name: "contribution"
+            },
+            {
+                type: "input",
+                message: "What are the test instructions?",
+                name: "test"
+            },
+            {
+                type: "input",
+                message: "What is your Github repository link?",
+                name: "repository"
+            },
+            {
+                type: "list",
+                message: "What type of license do you want to add to your readme?",
+                choices: [" MIT", " Apache 2.0", " GNU v3.0"],
+                name: "license"
+            }
+        ])
+        .then(function(response) {
+            console.log(response);
+            let myHtmlDoc = generateMarkdown(response);
+            fs.writeFile("README.md", myHtmlDoc, function (err) {
+                if (err) {
+                    return console.log(err);
+                }
+                console.log("Success!");
+            });
+        });
 }
-
-// function call to initialize program
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,38 @@
 // function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown(response) {
+  return `#${response.title}
+
+  ## Description
+  ${response.description}
+
+  ## Table of Contents
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Contributions](#contributing)
+  * [Tests](#tests)
+  * [Licenses](#licenses)
+  * [Badges](#badges)
+
+  ## Installation
+  * If you need to pull my code for this homework, please visit my Github repository at: ${response.repository}
+
+  ## Usage
+  <img src="./Assets/${response.usage}">
+
+  ## Contributing
+  ${response.contribution}
+
+  ## Tests
+  ${response.test}
+
+  ## Licenses
+  ${response.license}
+
+  ## Badges
+  ![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath)
+
+  © 2020 ${response.name} | UW-SEA-FSF-PT-08-2020-U-C-TTh
 
 `;
 }
